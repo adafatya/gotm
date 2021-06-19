1564fcd4098e49d9208e584f88c33cb4e0ca5891
adafatya <aufaditya1895@gmail.com>
2021-06-19 21:16:56 +0700

    remove comment debugging but pow still bugged

--- lib/pow/pow.go
+++ lib/pow/pow.go
@@ -1,7 +1,7 @@
 package pow
 
 import (
-	// "time"
+	"time"
 	"strings"
 )
 
@@ -30,7 +30,7 @@ func Pow(n int, m int) {
 
 func q0(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q0")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'X') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q1(s, pos+1)
@@ -47,7 +47,7 @@ func q0(s string, pos int) {
 
 func q1(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q1")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q1(s, pos+1)
 	} else if(s[pos] == '1') {
@@ -59,7 +59,7 @@ func q1(s string, pos int) {
 
 func q2(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q2")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'X') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q3(s, pos+1)
@@ -75,7 +75,7 @@ func q2(s string, pos int) {
 
 func q3(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q3")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q3(s, pos+1)
 	} else if(s[pos] == 'B') {
@@ -88,7 +88,7 @@ func q3(s string, pos int) {
 
 func q4(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q4")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q4(s, pos-1)
 	} else if(s[pos] == 'B') {
@@ -100,7 +100,7 @@ func q4(s string, pos int) {
 
 func q5(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q5")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '1') {
 		q6(s, pos-1)
 	} else {
@@ -110,7 +110,7 @@ func q5(s string, pos int) {
 
 func q6(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q6")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q6(s, pos-1)
 	} else if(s[pos] == 'B') {
@@ -122,7 +122,7 @@ func q6(s string, pos int) {
 
 func q7(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q7")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q7(s, pos+1)
 	} else if(s[pos] == '1') {
@@ -134,7 +134,7 @@ func q7(s string, pos int) {
 
 func q8(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q8")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q8(s, pos+1)
 	} else if(s[pos] == '0') {
@@ -147,7 +147,7 @@ func q8(s string, pos int) {
 
 func q9(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q9")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q9(s, pos-1)
 	} else if(s[pos] == '1') {
@@ -159,7 +159,7 @@ func q9(s string, pos int) {
 
 func q10(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q10")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		q10(s, pos-1)
 	} else if(s[pos] == '0') {
@@ -174,7 +174,7 @@ func q10(s string, pos int) {
 
 func q11(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q11")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		q11(s, pos+1)
 	} else if(s[pos] == '1') {
@@ -186,7 +186,7 @@ func q11(s string, pos int) {
 
 func q12(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q12")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q12(s, pos+1)
 	} else if(s[pos] == 'X') {
@@ -198,7 +198,7 @@ func q12(s string, pos int) {
 
 func q13(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q13")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q13(s, pos+1)
 	} else if(s[pos] == 'B') {
@@ -211,7 +211,7 @@ func q13(s string, pos int) {
 
 func q14(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q14")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q14(s, pos-1)
 	} else if(s[pos] == 'X') {
@@ -223,7 +223,7 @@ func q14(s string, pos int) {
 
 func q15(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q15")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q15(s, pos-1)
 	} else if(s[pos] == '1') {
@@ -235,7 +235,7 @@ func q15(s string, pos int) {
 
 func q16(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q16")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		s = s[:pos]+"0"+s[pos+1:]
 		q16(s, pos+1)
@@ -248,7 +248,7 @@ func q16(s string, pos int) {
 
 func q17(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q17")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"Z"+s[pos+1:]
 		q18(s, pos+1)
@@ -259,7 +259,7 @@ func q17(s string, pos int) {
 
 func q18(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q18")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '1') {
 		q19(s, pos+1)
 	} else {
@@ -269,7 +269,7 @@ func q18(s string, pos int) {
 
 func q19(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q19")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q19(s, pos+1)
 	} else if(s[pos] == '0') {
@@ -284,7 +284,7 @@ func q19(s string, pos int) {
 
 func q20(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q20")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q20(s, pos-1)
 	} else if(s[pos] == '1') {
@@ -296,7 +296,7 @@ func q20(s string, pos int) {
 
 func q21(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q21")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		q21(s, pos-1)
 	} else if(s[pos] == '0') {
@@ -311,7 +311,7 @@ func q21(s string, pos int) {
 
 func q22(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q22")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		q22(s, pos+1)
 	} else if(s[pos] == '1') {
@@ -323,7 +323,7 @@ func q22(s string, pos int) {
 
 func q23(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q23")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q23(s, pos+1)
 	} else if(s[pos] == 'X') {
@@ -335,7 +335,7 @@ func q23(s string, pos int) {
 
 func q24(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q24")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'Z') {
 		q24(s, pos+1)
 	} else if(s[pos] == 'B') {
@@ -347,7 +347,7 @@ func q24(s string, pos int) {
 
 func q25(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q25")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		q25(s, pos-1)
 	} else if(s[pos] == '0') {
@@ -362,7 +362,7 @@ func q25(s string, pos int) {
 
 func q26(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q26")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'Z') {
 		q26(s, pos+1)
 	} else if(s[pos] == 'B') {
@@ -375,7 +375,7 @@ func q26(s string, pos int) {
 
 func q27(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q27")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q27(s, pos-1)
 	} else if(s[pos] == 'Z') {
@@ -387,7 +387,7 @@ func q27(s string, pos int) {
 
 func q28(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q28")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		s = s[:pos]+"0"+s[pos+1:]
 		q28(s, pos+1)
@@ -400,7 +400,7 @@ func q28(s string, pos int) {
 
 func q29(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q29")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q29(s, pos+1)
 	} else if(s[pos] == 'X') {
@@ -412,7 +412,7 @@ func q29(s string, pos int) {
 
 func q30(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q30")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'Z') {
 		s = s[:pos]+"0"+s[pos+1:]
 		q30(s, pos+1)
@@ -425,7 +425,7 @@ func q30(s string, pos int) {
 
 func q31(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q31")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0' || s[pos] == 'B') {
 		q31(s, pos-1)
 	} else if(s[pos] == '1') {
@@ -437,7 +437,7 @@ func q31(s string, pos int) {
 
 func q32(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q32")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q32(s, pos-1)
 	} else if(s[pos] == '1') {
@@ -450,7 +450,7 @@ func q32(s string, pos int) {
 
 func q33(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q33")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'Z') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q34(s, pos+1)
@@ -461,7 +461,7 @@ func q33(s string, pos int) {
 
 func q34(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q34")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q34(s, pos-1)
@@ -476,7 +476,7 @@ func q34(s string, pos int) {
 
 func q35(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q35")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		q35(s, pos+1)
 	} else if(s[pos] == 'B') {
@@ -488,7 +488,7 @@ func q35(s string, pos int) {
 
 func q36(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q36")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q36(s, pos+1)
@@ -502,7 +502,7 @@ func q36(s string, pos int) {
 
 func q37(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q37")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'X' || s[pos] == 'Y' || s[pos] == 'B') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q38(s, pos+1)
@@ -513,7 +513,7 @@ func q37(s string, pos int) {
 
 func q38(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q38")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q38(s, pos+1)
@@ -527,7 +527,7 @@ func q38(s string, pos int) {
 
 func q39(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q39")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '1') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q40(s, pos-1)
@@ -538,7 +538,7 @@ func q39(s string, pos int) {
 
 func q40(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q40")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q40(s, pos-1)
@@ -552,7 +552,7 @@ func q40(s string, pos int) {
 
 func q41(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q41")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '1') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q42(s, pos-1)
@@ -563,7 +563,7 @@ func q41(s string, pos int) {
 
 func q42(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q42")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q42(s, pos-1)
@@ -576,7 +576,7 @@ func q42(s string, pos int) {
 
 func q43(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q43")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q43(s, pos+1)
 	} else if(s[pos] == '0') {
@@ -589,7 +589,7 @@ func q43(s string, pos int) {
 
 func q44(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q44")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == '0') {
 		s = s[:pos]+"B"+s[pos+1:]
 		q44(s, pos+1)
@@ -603,7 +603,7 @@ func q44(s string, pos int) {
 
 func q45(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q45")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		s = s[:pos]+"X"+s[pos+1:]
 		q46(s, pos+1)
@@ -614,7 +614,7 @@ func q45(s string, pos int) {
 
 func q46(s string, pos int) {
 	println(s[:pos], ">", s[pos:], "\tstate = q46")
-	// time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	if(s[pos] == 'B') {
 		q47(s, pos+1)
 	} else {
